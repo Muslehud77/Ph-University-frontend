d4814f598ba849b5ff96f428c76eb654f6d73b7a
Muslehud77 <fardinmohit@gmail.com>
2024-07-08 22:31:41 +0800

    made a fokira functional login with token decode utils

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,55 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks/reduxHooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+
+
+type TLoginData = {
+  userId:string,password:string
+}
 
 const Login = () => {
+const dispatch = useAppDispatch()
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "SecurePassword123",
+    },
+  });
+
+  const [login,{error,isLoading}] = useLoginMutation()
+ 
+
+  const onSubmit = async (formData: TLoginData) => {
+    const userInfo = {
+      id: formData.userId,
+      password: formData.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+
+    const user = verifyToken(res.data.accessToken)
+   
+    
+    dispatch(setUser({user,token:res.data.accessToken}))
+  };
+
   return (
-    <div>
-     <h1>This is Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button disabled={isLoading} htmlType="submit">Login</Button>
+    </form>
   );
 };
 
